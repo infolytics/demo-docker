b943a2054dd037fda3b144d346139a9c87d8ff24
infolytics <narayanan@infolyticsglobal.com>
2017-07-18 00:13:52 +0800

    Update create_airlines_demo.sh

--- notebook-demo-docker/demo/packages/utils/create_airlines_demo.sh
+++ notebook-demo-docker/demo/packages/utils/create_airlines_demo.sh
@@ -3,5 +3,5 @@ set -e
 cd ~/
 
 cat ~/utils/create_table_flights.txt | ~/mapd/bin/mapdql -p HyperInteractive
-echo "COPY FLIGHTS FROM '~/utils/allyears_modified.csv' with (header = 'true', quoted='false', nulls='NA');" | ~/mapd/bin/mapdql -p HyperInteractive
+echo "COPY FLIGHTS FROM '~/allyears_modified.csv' with (header = 'true', quoted='false', nulls='NA');" | ~/mapd/bin/mapdql -p HyperInteractive
 rm allyears_modified.csv
